ab3b18cd1887ae5e29b738163fc5e022a5a3526d
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-23 19:52:52 -0300

    External packages

--- 1 - Packages/main.go
+++ 1 - Packages/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"packages/auxpack"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hi...")
 	auxpack.Write()
+
+	err := checkmail.ValidateFormat("matheusrf96@gmail.com")
+	fmt.Println(err)
 }
