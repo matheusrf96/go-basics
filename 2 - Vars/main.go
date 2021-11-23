ae2b36dcdc6cf1cb5a3153ce108d8360bd29a4a8
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-23 20:03:18 -0300

    Vars and consts

---
+++ 2 - Vars/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var var1 string = "Var 1"
+	fmt.Println(var1)
+
+	var2 := "Var 2"
+	fmt.Println(var2)
+
+	var (
+		var3 string = "Var 3"
+		var4 string = "Var 4"
+	)
+	fmt.Println(var3, var4)
+
+	var5, var6 := "Var 5", "Var 6"
+	fmt.Println(var5, var6)
+
+	const const1 = "Const 1"
+	fmt.Println(const1)
+
+	var5, var6 = var6, var5
+	fmt.Println(var5, var6)
+}
