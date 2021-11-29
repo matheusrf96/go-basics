a6ee15a8d93972d0575c9749263606e7e163c758
Matheus Rodrigues <matheusrf96@gmail.com>
2021-11-29 17:28:25 -0300

    Arrays and Slices

---
+++ 9 - Arrays and Slices/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var arr1 [5]string
+	arr1[0] = "Posição 1"
+	fmt.Println(arr1)
+
+	arr2 := [5]string{"index1", "index2", "index3", "index4", "index5"}
+	fmt.Println(arr2)
+
+	arr3 := [...]int{1, 2, 3}
+	fmt.Println(arr3)
+
+	slice := []int{11, 12, 13, 14, 15}
+	fmt.Println(slice)
+
+	slice = append(slice, 16)
+	fmt.Println(slice)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(arr3))
+
+	slice2 := arr2[1:3]
+	fmt.Println(slice2)
+
+	arr2[1] = "altered index"
+	fmt.Println(slice2)
+}
