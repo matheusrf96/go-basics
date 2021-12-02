a763184429a091c4138c34873730414288f72d82
Matheus Rodrigues <matheusrf96@gmail.com>
2021-12-02 17:20:02 -0300

    Parallel tests

--- 22 - Tests/1 - Intro/addresses/addresses_test.go
+++ 22 - Tests/1 - Intro/addresses/addresses_test.go
@@ -8,14 +8,16 @@ type testCase struct {
 }
 
 func TestAddresType(t *testing.T) {
+	t.Parallel()
+
 	testCases := []testCase{
 		{"Street ABC", "Street"},
 		{"Avenue Fábio Santos", "Avenue"},
 		{"Road ZXY", "Road"},
-		{"Plaza A", "Street"},
-		{"", "Street"},
-		{"Road 123", "Street"},
-		{"Road 321", "ROAD"},
+		// {"Plaza A", "Street"},
+		// {"", "Street"},
+		// {"Road 123", "Street"},
+		// {"Road 321", "ROAD"},
 	}
 
 	for _, tc := range testCases {
@@ -23,11 +25,19 @@ func TestAddresType(t *testing.T) {
 
 		if receivedAddressType != tc.expectedReturn {
 			t.Errorf(
-				"The address received (%s) is different from the expected (%s). Case: %s",
+				"The address received (%s) is different from the expected (%s). Case: `%s`",
 				receivedAddressType,
 				tc.expectedReturn,
 				tc.insertedAddress,
 			)
 		}
+	}
+}
+
+func TestAny(t *testing.T) {
+	t.Parallel()
+
+	if 1 > 2 {
+		t.Error("1 is not greater than 2")
 	}
 }
