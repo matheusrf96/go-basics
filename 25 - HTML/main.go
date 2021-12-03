17f144a891f16e08f0b13933fac778b78721e124
Matheus Rodrigues <matheusrf96@gmail.com>
2021-12-03 12:01:32 -0300

    Go with HTML

--- 25 - HTML/main.go
+++ 25 - HTML/main.go
@@ -9,18 +9,8 @@ func root(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("root"))
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hi"))
-}
-
-func users(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("loading users page..."))
-}
-
 func main() {
 	http.HandleFunc("/", root)
-	http.HandleFunc("/home", home)
-	http.HandleFunc("/users", users)
 
 	log.Println("Server running at: 0.0.0.0:5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
